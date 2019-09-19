2649007bdcbc7b87d1f9655463a7e70e0202c3e8
Nikolas Haug <nikohaug21@gmail.com>
2019-09-19 11:41:11 -0500

    Updates

---
+++ precache-manifest.006e6207bd664d18147ad9c438d332e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54ddff65de707462b4daf5b4451b3e4",
+    "url": "/react_basic/index.html"
+  },
+  {
+    "revision": "44ca092326dd3747ced5",
+    "url": "/react_basic/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "bced947ef4ae0ed66063",
+    "url": "/react_basic/static/js/2.ebb8b90c.chunk.js"
+  },
+  {
+    "revision": "44ca092326dd3747ced5",
+    "url": "/react_basic/static/js/main.613796a3.chunk.js"
+  },
+  {
+    "revision": "0b63fec5dff8831cbd40",
+    "url": "/react_basic/static/js/runtime~main.c565b9ac.js"
+  }
+]);
